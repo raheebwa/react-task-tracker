de30671bdb3152c81f892eaf826e547795970ab9
Aheebwa Ramadhan <raheebwa@gmail.com>
2021-06-20 09:50:05 +0300

    feat: can add new task from form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks"
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
    const [tasks, setTasks] = useState([
@@ -35,9 +36,18 @@ function App() {
     const toggleReminder = (id) => {
       setTasks(tasks.map((task)=>task.id === id ? { ...task, reminder: !task.reminder} : task))
     }
+
+    // Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random()*1000) + 1
+        const newTask = {id, ...task}
+        setTasks([...tasks, newTask])
+    }
+
   return (
     <div className="container">
       <Header />
+      <AddTask  onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask}
       onToggle={toggleReminder} /> : 'No Tasks to Show'}
     </div>
